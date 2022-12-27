38dcca35ba2adc2a6b33c0bffbe8c1e3946cfcef
EkhlassHijjo <ekhlass.hijjo94@gmail.com>
2022-12-27 08:42:54 +0200

    create edit note view

---
+++ lib/views/widgets/edit_note_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
+
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+      ],
+    );
+  }
+}
