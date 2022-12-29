2caee0091dc6845965ac04b54a820d5434607368
EkhlassHijjo <ekhlass.hijjo94@gmail.com>
2022-12-29 11:59:52 +0200

    init Hive

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff62FCD7);
-
+const kNotesBox = 'notes_box'; //عبارة عن نوع بيانات constلل 
+                               // const String  مينفعش أكتب: اسم متغير 
+                               //وبعمل هيك لانو هستخدمه بكذا مكان المشروع 
